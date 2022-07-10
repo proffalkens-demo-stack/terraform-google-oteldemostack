4fce3f2514d18ebcabed3be0e2c2833be317b026
Matthew Macdonald-Wallace <matt@doics.co>
2022-07-10 21:29:33 +0100

    Format the provider config correctly

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 provider "google" {
-  project     = "oteldemostack"
-  region      = "us-central1"
+  project = "oteldemostack"
+  region  = "us-central1"
 }
 
 provider "google-beta" {
-  project     = "oteldemostack"
-  region      = "us-central1"
+  project = "oteldemostack"
+  region  = "us-central1"
 }
